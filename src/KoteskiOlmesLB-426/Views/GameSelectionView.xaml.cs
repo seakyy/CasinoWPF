06e1f1b75f949b9eb62eecff55e84abb0a5ab179
David Koteksi <142885534+seakyy@users.noreply.github.com>
2025-04-07 13:40:36 +0200

    v0.0.2
    - Can now choose if you want to play BlackJack or SlotMachine

--- src/KoteskiOlmesLB-426/Views/GameSelectionView.xaml.cs
+++ src/KoteskiOlmesLB-426/Views/GameSelectionView.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using KoteskiOlmesLB_426.ViewModels;
 using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -13,7 +15,32 @@ namespace KoteskiOlmesLB_426.Views
         public GameSelectionView()
         {
             InitializeComponent();
-            DataContext = new GameSelectionViewModel();
+
+            var viewModel = new GameSelectionViewModel();
+            viewModel.NavigationRequested += ViewModel_NavigationRequested;
+
+            DataContext = viewModel;
+        }
+
+        private void ViewModel_NavigationRequested(object sender, NavigationEventArgs e)
+        {
+            var mainWindow = System.Windows.Application.Current.MainWindow as MainWindow;
+
+            if (mainWindow != null && mainWindow.MainFrame != null)
+            {
+                switch (e.TargetView)
+                {
+                    case "BlackJack":
+                        mainWindow.MainFrame.Navigate(new BlackJackView());
+                        break;
+                    case "SlotMachine":
+                        mainWindow.MainFrame.Navigate(new SlotMachineView()); // Wenn du die View schon hast
+                        break;
+                    case "MainMenu":
+                        mainWindow.MainFrame.Navigate(new StartPage());
+                        break;
+                }
+            }
         }
     }
 }
