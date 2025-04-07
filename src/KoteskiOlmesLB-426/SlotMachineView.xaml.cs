e235c5fd33aa1d816f4d5f3a03eac7c5221dd01b
David Koteksi <142885534+seakyy@users.noreply.github.com>
2025-04-07 19:43:34 +0200

    Corrected SlotMachineView.xaml.cs
    Changed that line of code from: var viewModel = new BlackJackViewModel();
    to: var viewModel = new SlotMachineViewModel();

---
+++ src/KoteskiOlmesLB-426/SlotMachineView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using KoteskiOlmesLB_426.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using KoteskiOlmesLB_426.ViewModels;
+
+namespace KoteskiOlmesLB_426.Views
+{
+    public partial class SlotMachineView : Page
+    {
+        public SlotMachineView()
+        {
+
+            InitializeComponent();
+
+            var viewModel = new SlotMachineViewModel();
+            viewModel.NavigationRequested += ViewModel_NavigationRequested;
+
+            DataContext = viewModel;
+        }
+
+        private void ViewModel_NavigationRequested(object sender, NavigationEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow == null || mainWindow.MainFrame == null)
+                return;
+
+            switch (e.TargetView)
+            {
+                case "GameSelection":
+                    mainWindow.MainFrame.Navigate(new GameSelectionView());
+                    break;
+                case "MainMenu":
+                    mainWindow.MainFrame.Navigate(new StartPage());
+                    break;
+            }
+        }
+
+        private void SpinButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Add logic for spinning the slot machine
+        }
+    }
+}
