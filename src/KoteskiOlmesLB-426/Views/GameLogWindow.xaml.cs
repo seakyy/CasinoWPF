2b9c294fa6d1a0f9a3a896d611ab395d30a5de11
David Koteksi <142885534+seakyy@users.noreply.github.com>
2025-04-24 20:25:37 +0200

    v0.6.0 - Download Button and fix in .csproj
    Implemented Download Button for GameLog - It will now download your game logs as a JSON file

--- src/KoteskiOlmesLB-426/Views/GameLogWindow.xaml.cs
+++ src/KoteskiOlmesLB-426/Views/GameLogWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using KoteskiOlmesLB_426.Services;
 
 namespace KoteskiOlmesLB_426.Views
@@ -10,6 +11,29 @@ namespace KoteskiOlmesLB_426.Views
             InitializeComponent();
             DataContext = GameLogService.Instance;
         }
+
+        private void SaveGameLog_Click(object sender, RoutedEventArgs e)
+        {
+            var log = GameLogService.Instance.GameLogEntries;
+
+            var dialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "JSON-Datei (*.json)|*.json",
+                FileName = "GameLog.json"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                var json = System.Text.Json.JsonSerializer.Serialize(log, new System.Text.Json.JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+                File.WriteAllText(dialog.FileName, json);
+                MessageBox.Show("Spielverlauf gespeichert!", "Export erfolgreich", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
     }
 }
 
